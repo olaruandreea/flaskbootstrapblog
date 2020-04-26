bb6e41453b47743a30eafa0b655ed63733459742
olaru <olaruandreeapaticia@gmail.com>
2020-04-26 14:53:29 +0100

    new template

--- run.py
+++ run.py
@@ -8,5 +8,14 @@ Bootstrap(app)
 def home():
     return render_template("home.html")
 
+@app.route('/addpost')
+def add_post():
+    return render_template('addpost.html')
+
+
+@app.route('/aboutme')
+def about_me():
+    return render_template('aboutme.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
