43ac33d561d59261fe9968a8e080dcdd4c4334f9
olaru <olaruandreeapaticia@gmail.com>
2020-04-28 21:22:43 +0100

    Latest update

--- killprocess.sh
+++ killprocess.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# Script to kill python when the app is throwing "Address already in use"
+
 LINE=$(ps -ef | grep "python run.py")
 PROCESS=$(echo "$LINE" | awk '{print $2}')
 kill -9 $PROCESS
--- run.py
+++ run.py
@@ -1,5 +1,6 @@
-from flask import Flask, abort, flash, Markup, redirect, render_template,request
-from flask_sqlalchemy import SQLAlchemy, Response, session, url_for
+from flask import Flask, abort, flash, Markup, redirect
+from flask import render_template,request, Response, session, url_for
+from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 from datetime import datetime
 
@@ -25,23 +26,29 @@ def home():
     return render_template("home.html")
 
 @app.route('/addpost', methods=['GET','POST'])
-def addpost():
-    blog_title = request.form.get('inputTitle')
-    blog_author = request.form.get('blogAuthor')
-    blog_content = request.form.get('blogContent')
-    print blog_title
-    print blog_author
-    print blog_content
-    return render_template('addpost.html')
+def addPost():
+    if request.method == 'POST':
+        title = request.form['blogTitle']
+        return redirect(url_for('displayPost', post_title=title))
+    else:
+        return render_template('addpost.html')
 
-@app.route('/blog', methods=['GET'])
-def displayblog():
+@app.route('/blog')
+def displayBlog():
+    return "my blog page"
 
-    return render_template('blog.html')
+@app.route('/blog/<post_title>')
+def displayPost(post_title):
+    return post_title
 
 @app.route('/aboutme')
-def aboutme():
+def aboutMe():
     return render_template('aboutme.html')
+
+@app.route('/signup')
+def signUp():
+    return render_template('aboutme.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
